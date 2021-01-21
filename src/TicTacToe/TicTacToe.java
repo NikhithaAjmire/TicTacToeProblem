872846026f573a7d3cce1e81d03cc972d6a25c99
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-01-21 14:40:26 +0530

    ADDED:PrintingBoard

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -22,6 +22,18 @@ public class TicTacToe {
 	           
 	        }
 	    }
+	 public void printingBoard() {
+	        String horizPart = "+---+---+---+";
+	        for (int i = 0; i < 3; i++) {
+	            System.out.println(horizPart);
+	            for (int j = 1; j < 4; j++) {
+	                System.out.print("| " + board[i * 3 + j] + " ");
+	            }
+	            System.out.print("|\n");
+	        }
+	        System.out.print(horizPart);
+	        System.out.print("\n");
+	    }
 
 	public static void main(String[] args) {
 		System.out.println("---------Welcome to TicTacToe Problem-----------");
@@ -29,6 +41,7 @@ public class TicTacToe {
 		Scanner sc = new Scanner(System.in);
 		ticTacToeGame.creatingBoard();
 		ticTacToeGame.getUserInput(sc.next());
+		ticTacToeGame.printingBoard();
 
 	}
 
