f320db6e94c8611e01e4b995c17a931b37581b39
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-01-21 14:25:38 +0530

    ADDED:BoardInitialisation

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,9 +1,19 @@
 package TicTacToe;
 
 public class TicTacToe {
-
+	public char board[] = new char[10];
+	TicTacToe() {
+        creatingBoard();
+    }
+	 public void creatingBoard() {
+	        for (int i = 1; i < 10; i++) {
+	            board[i] = ' ';
+	        }
+	    }
 	public static void main(String[] args) {
 		System.out.println("---------Welcome to TicTacToe Problem-----------");
+		TicTacToe ticTacToeGame = new TicTacToe();
+		ticTacToeGame.creatingBoard();
 
 	}
 
